b4733d84a4a3f9ac2ff926d4b1429ae4a32e9039
BenjaminTheDeveloper <39067259+BenjaminTheDeveloper@users.noreply.github.com>
2020-08-11 22:50:27 -0600

    Assignment 07 Version

--- SimpleCalculator/SimpleCalculator/Program.cs
+++ SimpleCalculator/SimpleCalculator/Program.cs
@@ -6,15 +6,11 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Clean Calculator");
             var CalculatorDriver = new CalculatorDriver();
-
-
             CalculatorDriver.Run(); 
             Console.WriteLine("Thank you for Calculating!");
             Console.ReadKey();
-
-          
-
 
         }
     }
